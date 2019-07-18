cf45f1cbb9917d60a413bac064e4de2320c81ec9
Oleh Mordach <omordach@gmail.com>
2019-07-18 15:10:19 +0300

    Removed google tah manager

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
 importScripts("sw-toolbox.js");
-toolbox.precache(["index.html","assets/css/fontawesome-all.min.css","assets/css/main.css","images/bg.jpg","images/pic01.jpg","images/pic02.jpg","images/pic03.jpg","images/pic05.jpg","assets/css/images/arrows.swg","assets/css/images/overlay.png",]);
+toolbox.precache(["index.html","assets/css/fontawesome-all.min.css","assets/css/main.css","images/bg.jpg","images/pic01.jpg","images/pic02.jpg","images/pic03.jpg","images/pic05.jpg","assets/css/images/arrow.swg","assets/css/images/overlay.png",]);
 toolbox.router.get("images/*", toolbox.cacheFirst); toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5});
 
