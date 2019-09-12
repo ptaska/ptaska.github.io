1da5594d1702b28202a59d04c03601bfb7d81d4e
Oleh Mordach <omordach@gmail.com>
2019-09-12 11:38:21 +0300

    .

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
 importScripts("sw-toolbox.js");
-toolbox.precache(["index.html","assets/css/fontawesome-all.min.css","assets/css/main.css","images/background.webp","images/about-me.webp","images/help.webp","images/services.webp","images/therapy.webp","images/logo.webp","images/NataliaRainyk.webp","assets/css/images/overlay.webp",]);
+toolbox.precache(["index.html","assets/css/fontawesome-all.min.css","assets/css/main.css","images/background.jpg","images/about-me.jpg","images/help.jpg","images/services.jpg","images/therapy.jpg","images/logo.png","images/NataliaRainyk.png","assets/css/images/overlay.jpg",]);
 toolbox.router.get("images/*", toolbox.cacheFirst); toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5});
 
