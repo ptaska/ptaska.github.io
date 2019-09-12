eb66f660d06d00540b8e2ebf4975e9006fc63774
Oleh Mordach <omordach@gmail.com>
2019-09-12 12:18:46 +0300

    validate html

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
 importScripts("sw-toolbox.js");
-toolbox.precache(["index.html","assets/css/fontawesome-all.min.css","assets/css/main.css","images/background.jpg","images/about-me.jpg","images/help.jpg","images/services.jpg","images/therapy.jpg","images/logo.png","images/NataliaRainyk.png","assets/css/images/overlay.jpg",]);
+toolbox.precache(["index.html","assets/css/fontawesome-all.min.css","assets/css/main.css","images/background.jpg","images/about-me.jpg","images/help.jpg","images/services.jpg","images/therapy.jpg","images/logo.png","images/NataliaRainyk.png","assets/css/images/overlay.png",]);
 toolbox.router.get("images/*", toolbox.cacheFirst); toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5});
 
