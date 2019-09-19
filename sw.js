bc143c012c6161043ab2d492c39a1d1c4845a431
Oleh Mordach <omordach@gmail.com>
2019-09-19 12:05:08 +0300

    images

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
 importScripts("sw-toolbox.js");
-toolbox.precache(["index.html","assets/css/fontawesome-all.min.css","assets/css/main.css","images/background.jpg","images/about-me.jpg","images/help.jpg","images/services.jpg","images/therapy.jpg","images/logo.jpg","images/NataliaRainyk.jpg","assets/css/images/overlay.png",]);
+toolbox.precache(["index.html","assets/css/fontawesome-all.min.css","assets/css/main.css","images/background.jpg","images/about-me.jpg","images/services.jpg","images/therapy.jpg","images/logo.jpg","images/NataliaRainyk.jpg","assets/css/images/overlay.png",]);
 toolbox.router.get("images/*", toolbox.cacheFirst); toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5});
 
